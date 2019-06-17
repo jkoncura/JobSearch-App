b8256c26fb2e35439d2012f9bd0db94e7cc5cca9
Josip Koncurat <jkoncura@gmail.com>
2019-06-17 16:06:22 +0200

    creating custom login page

--- jobs-app/src/main/java/com/jobsearch/controller/JobController.java
+++ jobs-app/src/main/java/com/jobsearch/controller/JobController.java
@@ -1,3 +1,4 @@
+package com.jobsearch.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
