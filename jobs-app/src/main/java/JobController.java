58046a51436a2d224c560717068e90aa861f3039
Josip Koncurat <jkoncura@gmail.com>
2019-06-17 13:19:50 +0200

    added a controller, and a html file for the starting page

---
+++ jobs-app/src/main/java/JobController.java
@@ -0,0 +1,13 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+	@GetMapping(value ="/")
+	public String rootView() {
+		return "index"; // goes to index.html file, /src/main/resources/index.html
+		
+	}
+}
