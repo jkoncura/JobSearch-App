4a60ac3ead13fd3836af47304054a089992ba8c9
Josip Koncurat <jkoncura@gmail.com>
2019-06-18 09:15:25 +0200

    Adding password encoder

--- jobs-app/src/main/java/com/jobsearch/controller/JobController.java
+++ jobs-app/src/main/java/com/jobsearch/controller/JobController.java
@@ -1,4 +1,6 @@
 package com.jobsearch.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,5 +11,9 @@ public class JobController {
 	@GetMapping(value ="/")
 	public String rootView() {
 		return "index"; // goes to index.html file, /src/main/resources/index.html
+	}
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "dashboard";
 	}
 }
--- jobs-app/src/main/java/com/jobsearch/security/WebSecurityConfiguration.java
+++ jobs-app/src/main/java/com/jobsearch/security/WebSecurityConfiguration.java
@@ -1,19 +1,28 @@
 package com.jobsearch.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// who are you and prove it
 		auth.inMemoryAuthentication()
+			.passwordEncoder(getPasswordEncoder())
 			.withUser("jkoncura@gmail.com")
-			.password("password")
+			.password(getPasswordEncoder().encode("password"))
 			.roles("USER");
 	}
 	@Override
