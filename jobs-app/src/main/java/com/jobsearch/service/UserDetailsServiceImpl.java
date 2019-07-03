c25cf466953c4ffa564daecebc04963998f41d0c
Josip Koncurat <jkoncura@gmail.com>
2019-07-03 11:20:53 +0200

    get all the beans loaded by the Spring container

--- jobs-app/src/main/java/com/jobsearch/service/UserDetailsServiceImpl.java
+++ jobs-app/src/main/java/com/jobsearch/service/UserDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.jobsearch.domain.User;
@@ -15,6 +16,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
